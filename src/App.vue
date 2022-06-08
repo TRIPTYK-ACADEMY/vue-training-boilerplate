e1eed5acbd15f4f669e184b3e1103bb7f5964092
remadex <s.cardon@outlook.com>
2022-06-08 10:06:09 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,15 @@ const content = "Votre contenu ici !";
 </script>
 
 <template>
-  <div class="bg-slate-50 flex flex-col min-h-screen">
-    <header
-      class="mt-6 mx-8 shadow-md rounded-md flex justify-center bg-white overflow-hidden"
-    >
-      <img src="/logo.png" alt="logo" class="w-16 animation-image" />
-    </header>
-    <main class="container mx-auto m-8 border-2 border-dashed rounded p-8">
-      <!-- CONTENU ICI -->
-      {{ content }}
-    </main>
-  </div>
+<div class="bg-slate-50 flex flex-col min-h-screen">
+<header
+class="mt-6 mx-8 shadow-md rounded-md flex justify-center bg-white overflow-hidden"
+>
+<img src="/logo.png" alt="logo" class="w-16 animation-image" />
+</header>
+<main class="container mx-auto m-8 border-2 border-dashed rounded p-8">
+<!-- CONTENU ICI -->
+{{ content }}
+</main>
+</div>
 </template>
-
-<style>
-.animation-image {
-  @apply cursor-pointer duration-200;
-}
-
-.animation-image:hover {
-  @apply scale-75;
-}
-</style>
